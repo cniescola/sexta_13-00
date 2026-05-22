e15dd20ffe6d2c7b70130ea4d65742ab4694233b
thiago <thiagoobuenoo12@gmail.com>
2026-05-22 14:23:05 -0300

    javascript coleta de dados

--- Fundamentos/resources/views/home.blade.php
+++ Fundamentos/resources/views/home.blade.php
@@ -10,7 +10,7 @@
 
 <style>
     .card{
-        box-shadow: 6px 6px 10px 00000007
+        box-shadow: 6px 6px 10px 00000005
     }
 
     .botao{
@@ -59,12 +59,12 @@
 
                             <div class="col">
                                 <label class="form-label">Nome do Produto</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" id="NomeProd">
                             </div>
 
                             <div class="col">
                                 <label class="form-label">Codigo do Produto</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" id="CodigoProd">
                             </div>
     
                         </div>
@@ -73,24 +73,24 @@
 
                             <div class="col">
                                 <label class="form-label">Descrição do Produto</label>
-                                <textarea class="form-control" rows="7"></textarea>
+                                <textarea class="form-control" rows="7" id="DescProd"></textarea>
                             </div>
 
                             <div class="col">
 
                                 <div class="row m-0">
                                     <label class="form-label">Preço do Produto</label>
-                                    <input type="number" class="form-control">
+                                    <input type="number" class="form-control" id="PreProd">
                                 </div>
 
                                 <div class="row m-0">
                                     <label class="form-label">Preço da Entrega</label>
-                                    <input type="number" class="form-control">
+                                    <input type="number" class="form-control" id="PreEnt">
                                 </div>
 
                                 <div class="row m-0">
                                     <label class="form-label">Porcetagem de Desconto</label>
-                                    <input type="number" class="form-control">
+                                    <input type="number" class="form-control" id="PorcDes">
                                 </div>
 
                             </div>
@@ -104,7 +104,7 @@
                     <div class="col-4 card d-flex flex-column justify-content-center p-4">
 
                         <div class="m-4">
-                            <button class="btn btn-success w-100 p-3" style="border-radius: 50px;"> Salvar</button>
+                            <button class="btn btn-success w-100 p-3" style="border-radius: 50px;" id="Salvar"> Salvar</button>
                         </div>
 
                         <div class="m-4">
@@ -122,6 +122,22 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+
+        document.querySelector('#Salvar').addEventListener("click",function(){
+           var infnome = document.querySelector('#NomeProd').value;
+           var infcod = document.querySelector('#CodigoProd').value;
+           var infdesc = document.querySelector('#DescProd').value;
+           var infpre = document.querySelector('#PreProd').value;
+           var infent = document.querySelector('#PreEnt').value;
+           var infdes = document.querySelector('#PorcDes').value;
+            
+           console.log(infnome+"  "+infcod+"  "+infdesc+"  "+infpre+"  "+infent+"  "+infdes);
+        });
+
+    </script>
+
 </body>
 
 </html>
